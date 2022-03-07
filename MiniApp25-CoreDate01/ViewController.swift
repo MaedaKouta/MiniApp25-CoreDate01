0025ce0d4763db8a3676e866c715c11711a8e5d4
MaedaKouta <maedakoutaa@gmail.com>
2022-03-07 10:15:19 +0900

    Initial Commit

---
+++ MiniApp25-CoreDate01/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MiniApp25-CoreDate01
+//
+//  Created by 前田航汰 on 2022/03/07.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
